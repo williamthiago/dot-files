ba7f6cf7ce5d980b975283d4cd5add521bbd19b3
William Thiago <williamthiago@gmail.com>
2016-12-29 02:28:40 -0200

    Add windows 10 script install

---
+++ README.md
@@ -0,0 +1,7 @@
+Windows 10
+
+Powershell as administrator
+
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+./install-windows10.ps1
---
+++ install-windows10.ps1
@@ -0,0 +1,35 @@
+﻿# Install chocolatey
+iwr https://chocolatey.org/install.ps1 -UseBasicParsing | iex
+choco feature enable -n=allowGlobalConfirmation
+
+# Install developer tools
+choco install git
+choco install docker
+choco install docker-compose
+choco install notepadplusplus
+choco install vim
+choco install sublimetext3
+choco install visualstudiocode
+choco install dotnet4.5 
+choco install dotnetcore
+
+# Install browsers
+choco install googlechrome
+choco install firefox
+
+# Install tools
+choco install 7zip
+choco install procexp
+choco install procmon
+
+# Install apps
+choco install skype
+choco install slack
+choco install vlc
+choco install utorrent
+choco install steam
+choco install spotify
+choco install teamviewer 
+
+# Reload das configurações de ambiente
+refreshenv
